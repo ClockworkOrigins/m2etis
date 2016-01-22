3dc365e9cd8563d8ec29115fc80e719ed400caf8
Daniel Bonrath <bonne@clockwork-origins.de>
2016-01-22 15:01:13 +0100

    METIS-78 updated license

--- library/include/m2etis/config/DetMergeConfigTest.h
+++ library/include/m2etis/config/DetMergeConfigTest.h
@@ -1,12 +1,12 @@
-/**
- Copyright 2012 FAU (Friedrich Alexander University of Erlangen-Nuremberg)
- 
+/*
+ Copyright (2016) Michael Baer, Daniel Bonrath, All rights reserved.
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,11 +21,11 @@ namespace m2etis {
 namespace config {
 namespace order {
 
-struct DetMergeConfigTest {
-	static const unsigned int eps = 50;
-	static const unsigned int delta = 10;
-	static const unsigned int raster = 1000;
-};
+	struct DetMergeConfigTest {
+		static const unsigned int eps = 50;
+		static const unsigned int delta = 10;
+		static const unsigned int raster = 1000;
+	};
 
 } /* namespace order */
 } /* namespace config */
--- library/include/m2etis/message/info/order/NullOrderInfo.h
+++ library/include/m2etis/message/info/order/NullOrderInfo.h
@@ -1,5 +1,5 @@
 /*
- Copyright 2012 FAU (Friedrich Alexander University of Erlangen-Nuremberg)
+ Copyright (2016) Michael Baer, Daniel Bonrath, All rights reserved.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
